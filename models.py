d2d99efba84d8be35880006f04e8b8a55335b898
liq23wr liq23wr <75304546+pladee42@users.noreply.github.com>
2025-02-20 01:50:35 +0000

    update results

--- models.py
+++ models.py
@@ -11,8 +11,6 @@ def chat(model_name: str, query: str, hf_token: str = hf_token) -> str:
     """
     Download Open-source models from Hugging Face and Send the query to the model.
     """
-    # Time Tracking
-    start_time = time.time()  # Start time tracking
     
     # Check if CUDA is available, and move model to GPU if so
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")   
@@ -27,7 +25,10 @@ def chat(model_name: str, query: str, hf_token: str = hf_token) -> str:
                                                 token=hf_token).to(device)
     model.generation_config.pad_token_id = model.generation_config.eos_token_id
     print(f"\n[INFO] Using model: {model_name}")
-
+    
+    # Time Tracking
+    start_time = time.time()
+    
     # Test the model by generating text
     template = [
         {"role": "user",
@@ -42,10 +43,15 @@ def chat(model_name: str, query: str, hf_token: str = hf_token) -> str:
     # Tokenize the input text
     input_ids = tokenizer(prompt, return_tensors="pt").to(device)
 
+    
+    
     # Generate outputs passed on the tokenized input
     with torch.amp.autocast('cuda'):
         outputs = model.generate(**input_ids, max_new_tokens=4096)
 
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    
     # Decode and print the output text
     output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     if '</think>' in output_text:
@@ -53,8 +59,6 @@ def chat(model_name: str, query: str, hf_token: str = hf_token) -> str:
         
     print(f"\nResponse:\n{output_text}\n")
     
-    end_time = time.time()
-    elapsed_time = end_time - start_time
     print(f"[INFO] {model_name} took : {elapsed_time:.2f} seconds for response\n")  # Print elapsed time
     
     return output_text
